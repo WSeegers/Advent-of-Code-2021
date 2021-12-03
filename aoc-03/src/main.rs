da306132712078f987482a6fb90d782155a9ea6d
Werner Seegers <werner.seegers@structureit.net>
2021-12-03 17:19:28 +0200

    Clean up aoc-03

--- aoc-03/src/main.rs
+++ aoc-03/src/main.rs
@@ -1,7 +1,4 @@
-struct BitSummary {
-    pub sample_size: usize,
-    pub break_down: Vec<u32>,
-}
+use itertools::Itertools;
 
 fn get_input() -> impl Iterator<Item = u16> {
     aoc_common::read_lines("resources/aoc-03.input")
@@ -10,45 +7,31 @@ fn get_input() -> impl Iterator<Item = u16> {
         .map(|line| u16::from_str_radix(&line.unwrap(), 2).unwrap())
 }
 
-fn bit_count<T>(list: T, bit_width: usize) -> BitSummary
+fn part_1<T>(input: T, bit_width: usize) -> u64
 where
     T: IntoIterator<Item = u16>,
 {
-    let selector_mask = 1 << (bit_width - 1);
-    let summary = BitSummary {
-        sample_size: 0,
-        break_down: vec![0u32; bit_width],
-    };
+	let selector_mask = 1 << (bit_width - 1);
 
-    list.into_iter().fold(summary, |mut acc, line| {
-        acc.break_down
-            .iter_mut()
-            .enumerate()
-            .for_each(|(i, v)| *v = *v + ((line & (selector_mask >> i)) > 0) as u32);
-        acc.sample_size += 1;
-        acc
-    })
-}
+    let  (len, groups) = input
+        .into_iter()
+        .fold((0, vec![0u32; bit_width]), |(len, mut groups), line| {
+            groups
+                .iter_mut()
+                .enumerate()
+                .for_each(|(i, v)| *v = *v + ((line & (selector_mask >> i)) > 0) as u32);
+            (len + 1, groups)
+        });
 
-fn part_1<T>(input: T, bit_width: usize) -> u32
-where
-    T: IntoIterator<Item = u16>,
-{
-    let padding_mask = !(u32::MAX << bit_width);
-    let bit_summary = bit_count(input, bit_width);
-
-    let half_len: u32 = (bit_summary.sample_size / 2).try_into().unwrap();
-
-    let val = bit_summary
-        .break_down
-        .iter()
-        .map(|&v| v > half_len)
-        .fold(0, |acc, val| (acc << 1) + val as u32);
+    let val = groups
+        .into_iter()
+        .map(|v| (v > len - v) as u64)
+        .fold(0, |acc, val| (acc << 1) + val);
 
     let gamma = val;
-    let eps = !val & padding_mask;
+    let eps = !val & !(u64::MAX << bit_width);
 
-    return gamma * eps;
+	return gamma * eps;
 }
 
 fn part_2<T>(input: T, bit_width: usize) -> u64
@@ -56,56 +39,65 @@ where
     T: IntoIterator<Item = u16>,
 {
     let input_list = input.into_iter().collect::<Vec<_>>();
-    let mut _input = input_list.clone();
+    let mut working_list = input_list.clone();
 
     for i in 0..bit_width {
         let selector_mask = 1 << (bit_width - 1 - i);
 
-        let count = _input.iter().filter(|&x| (x & selector_mask) != 0).count();
-        let logic = selector_mask * (count >= _input.len() - count) as u16;
+        working_list = working_list
+            .into_iter()
+            .into_group_map_by(|&x| ((x & selector_mask) != 0) as u8)
+            .into_iter()
+            .max_by(|(b1, v1), (b2, v2)| {
+                let len_cmp = v1.len().cmp(&v2.len());
+                match len_cmp {
+                    std::cmp::Ordering::Equal => b1.cmp(b2),
+                    _ => len_cmp,
+                }
+            })
+            .unwrap()
+            .1;
 
-        _input = _input
-            .iter()
-            .filter(|&&x| (x & selector_mask) == logic)
-            .map(|&x| x)
-            .collect::<Vec<_>>();
-
-    	if _input.len() <= 1 {
-    		break;
-    	}
+        if working_list.len() <= 1 {
+            break;
+        }
     }
 
-    let oxy = _input[0];
+    let oxy = working_list[0];
 
-	let mut _input = input_list;
-	for i in 0..bit_width {
+    let mut working_list = input_list;
+    for i in 0..bit_width {
         let selector_mask = 1 << (bit_width - 1 - i);
 
-        let count = _input.iter().filter(|&x| (x & selector_mask) != 0).count();
+        working_list = working_list
+            .into_iter()
+            .into_group_map_by(|&x| ((x & selector_mask) != 0) as u8)
+            .into_iter()
+            .min_by(|(b1, v1), (b2, v2)| {
+                let len_cmp = v1.len().cmp(&v2.len());
+                match len_cmp {
+                    std::cmp::Ordering::Equal => b1.cmp(b2),
+                    _ => len_cmp,
+                }
+            })
+            .unwrap()
+            .1;
 
-        let logic = selector_mask * (count < _input.len() - count) as u16;
-
-        _input = _input
-            .iter()
-            .filter(|&&x| (x & selector_mask) == logic)
-            .map(|&x| x)
-            .collect::<Vec<_>>();
-
-    	if _input.len() <= 1 {
-    		break;
-    	}
+        if working_list.len() <= 1 {
+            break;
+        }
     }
-    let co2 = _input[0];
+    let co2 = working_list[0];
 
     oxy as u64 * co2 as u64
 }
 
 fn main() {
     let part_1_solution = part_1(get_input(), 12);
-    println!("Part 1: {}", part_1_solution);
+    println!("Part 1: {}", part_1_solution); // 3687446
 
-	let part_2_solution = part_2(get_input(), 12);
-    println!("Part 2: {}", part_2_solution);
+    let part_2_solution = part_2(get_input(), 12);
+    println!("Part 2: {}", part_2_solution); // 4406844
 }
 
 #[cfg(test)]
@@ -146,7 +138,7 @@ mod tests {
         assert_eq!(actual, expected);
     }
 
-	#[test]
+    #[test]
     fn part_2_basic_2() {
         let ox = 0b00011;
         let co2 = 0b11111;
